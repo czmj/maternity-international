73486b39991c86cce893c903dd3e377261349680
Clara Jordan <clara.jordan@hpc.tools>
2022-04-23 19:58:41 +0100

    Change servies style to match homepage more

--- src/pages/services.js
+++ src/pages/services.js
@@ -2,7 +2,6 @@ import { graphql } from "gatsby"
 import get from "lodash/get"
 import React from "react"
 import Container from "../components/container"
-import Divider from "../components/divider"
 import Hero from "../components/hero"
 import Layout from "../components/layout"
 import FullWidthCard from "../sections/full-width-card"
@@ -18,31 +17,32 @@ class ServicesPage extends React.Component {
           subheading="Burrow under covers stinky cat, yet i dreamt about fish yum!. Bleghbleghvomit my furball really tie the room together scratch, and refuse to drink water except out of someone's glass massacre a bird in the living room and then look like the cutest and most innocent animal on the planet"
           image="https://placekitten.com/1800/900"
         />
-        <Container>
-          <ul className="list-unstyled">
-            {services
-              .sort((a, b) => a.order - b.order)
-              .map((service, index) => {
-                return (
-                  <>
-                    <li key={service.slug} id={service.slug}>
-                      <FullWidthCard
-                        direction={index % 2 ? "right" : "left"}
-                        icon={service.icon}
-                        title={service.name}
-                        description={
-                          service.longDescription.childMarkdownRemark.html
-                        }
-                        links={service.link}
-                        media={service.media}
-                      />
-                    </li>
-                    {index < services.length - 1 && <Divider />}
-                  </>
-                )
-              })}
-          </ul>
-        </Container>
+        <ul className="list-unstyled">
+          {services
+            .sort((a, b) => a.order - b.order)
+            .map((service, index) => {
+              return (
+                <li
+                  key={service.slug}
+                  id={service.slug}
+                  className={index % 2 ? "light-grey" : ""}
+                >
+                  <Container>
+                    <FullWidthCard
+                      direction={index % 2 ? "right" : "left"}
+                      icon={service.icon}
+                      title={service.name}
+                      description={
+                        service.longDescription.childMarkdownRemark.html
+                      }
+                      links={service.link}
+                      media={service.media}
+                    />
+                  </Container>
+                </li>
+              )
+            })}
+        </ul>
       </Layout>
     )
   }
